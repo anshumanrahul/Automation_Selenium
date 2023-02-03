c0012e3c2e1c4f4763666d0c9d98d6b0b956682b
RAHUL <RAHUL@LAPTOP-V17Q6KVC>
2023-02-03 13:17:36 +0530

    Program to find Even numbers between 1 to 100.

---
+++ Automation_Selenium/src/test/java/org/AutomationPractice/Automation_Selenium/ProgramToFindEvenNumbers.java
@@ -0,0 +1,16 @@
+package org.AutomationPractice.Automation_Selenium;
+
+public class ProgramToFindEvenNumbers {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++)
+		{
+			if (i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
