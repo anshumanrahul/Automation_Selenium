c67ea5fbb1b9332b687a76e8b54f166963866c39
RAHUL <RAHUL@LAPTOP-V17Q6KVC>
2023-02-12 17:15:26 +0530

    Polymorphism Concepts with Example

--- Automation_Selenium/src/test/java/inheritance_Abstraction_Polymorphism/AnimalClass_Method_Overriding.java
+++ Automation_Selenium/src/test/java/inheritance_Abstraction_Polymorphism/AnimalClass_Method_Overriding.java
@@ -1,7 +1,8 @@
 package inheritance_Abstraction_Polymorphism;
 
 public class AnimalClass_Method_Overriding {
-
+/* When a superclass and subclass has exactly the same name with same arguments then the method from child class 
+ overrides the method of the parent class.*/
 	public void WhoIAm()
 	{
 		System.out.println("I am an Animal");
--- Automation_Selenium/src/test/java/inheritance_Abstraction_Polymorphism/Cat_MethodOverriding.java
+++ Automation_Selenium/src/test/java/inheritance_Abstraction_Polymorphism/Cat_MethodOverriding.java
@@ -1,6 +1,6 @@
 package inheritance_Abstraction_Polymorphism;
 
-public class Cat_MethodOverriding {
+public class Cat_MethodOverriding extends AnimalClass_Method_Overriding {
 	public void WhoIAm()
 	{
 		System.out.println("I am a Cat");
